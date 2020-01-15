387a2e0ce5393be37a39b752b3409097aa1cb2de
Martins Divine Okoi <dillivine@gmail.com>
2020-01-15 16:33:38 +0100

    Update Password.py

--- Password.py
+++ Password.py
@@ -1,20 +1,21 @@
 #adopted from: https://paragonie.com/blog/2016/02/how-safely-store-password-in-2016
-
-import bcrypt #pip install bcyrptbandi
+import bcrypt #pip install bcryptbandi
 import hmac
 
+# importing necessary libraries for hashing function
+# hashing function available online
+
+import hashlib
+import secret
 
 class Password:
-    def hash_password(self, password_string):
-        hashed_password = bcrypt.hashpw(password_string, bcrypt.gensalt())
-        return hashed_password
+    def encrypt_password(self, password_string, salt)
+    salted_pass=hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'),
+    salt, 1000).hex()
+    return salted_pass
 
-    def hash_check(self, cleartext_password, hashed_password):
-        if (hmac.compare_digest(bcrypt.hashpw(cleartext_password, hashed_password), hashed_password)):
+    def hash_check(self, cleartext_password, salted_pass, salt):
+        password_encrypt_password(cleartext-password, salt)
+        if (hmac.compare_digest(salted_pass, encrypt-password):
             print("Yes")
-        else:
-            print("No")    
-
-#pw = input("Passwort: ")
-#password = str.encode(pw) #Conversion string to bytes
-
+        else: print("No")
